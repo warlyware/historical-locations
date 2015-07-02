20864602eb51a6a9c2fcd089141c373fbe9795bd
warlyware <danward@gmail.com>
2015-07-01 18:00:17 -0700

    Add error checking to API post method

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -2,21 +2,22 @@ var express = require('express');
 var router = express.Router();
 var Firebase = require('firebase');
 var fbRef =  new Firebase('https://historic-locations.firebaseio.com/locations');
-
-var fakeData = [
-  {name: 'location', description: 'Location1', year: '1996'},
-  {name: 'location', description: 'Location2', year: '1999'},
-  {name: 'location', description: 'Location3', year: '1992'},
-  {name: 'location', description: 'Location4', year: '1969'},    
-]
+var _ = require('lodash');
 
 router.get('/locations', function(req, res) {
-  res.json({locations: fakeData});
+  fbRef.once('value', function(snap) {
+    res.json({locations: snap.val()});
+  });
 });
 
 router.post('/locations', function(req, res) {
-  fbRef.push(req.body);
-  res.json(req.body);
+  var newLocation = _.pick(req.body, ['name', 'description', 'year']);
+  if (newLocation.name) {
+    fbRef.push(newLocation, function() {
+      res.json(newLocation);
+    });
+  }
+  res.status(404);
 });
 
 module.exports = router;
